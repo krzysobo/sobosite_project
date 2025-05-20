f90a861270ff023a65bbf0b708b0731c520023ef
Krzysztof Sobolewski <krzysztof.sobolewski@gmail.com>
2025-05-20 03:02:27 +0200

    RE #password update via admin fix

--- backend/user_forms/admin_views.py
+++ backend/user_forms/admin_views.py
@@ -13,6 +13,7 @@ from rest_framework.filters import OrderingFilter
 import user_forms.perms as perms
 import user_forms.serializers as sers  
 import user_forms.models as models
+import user_forms.utils as utils
 
 
 class AdminUserList(ListAPIView, APIView):
@@ -28,19 +29,22 @@ class AdminUserList(ListAPIView, APIView):
     
     def list(self, request, *args, **kwargs):
         resp = super(AdminUserList, self).list(request, *args, **kwargs)
-        return Response({"data": resp.data})
+        return utils.uni_response(data=resp.data)
 
 
 class AdminUserOpsCreate(APIView):
     permission_classes = [perms.PermIsAuth & perms.PermIsAdmin]    
     def post(self, request, format=None):
         ser = sers.CreateUserSerializerForAdminPanels(data=request.data)
-        ser.is_valid(raise_exception=True)
-        # return Response(ser.data)
+        try:
+            ser.is_valid(raise_exception=True)
+        except Exception:
+            return utils.uni_response(data=ser.data, errors=utils.serialize_error_data(ser.errors), status=400)
         instance = ser.save()
         instance.set_password(ser.validated_data['password'])
         instance.save()
-        return Response(sers.UserSerializerForAdminPanels(instance).data)
+        return utils.uni_response(data=sers.UserSerializerForAdminPanels(instance).data)
+        # return Response(sers.UserSerializerForAdminPanels(instance).data)
 
 
 class AdminUserOps(APIView):
@@ -55,23 +59,32 @@ class AdminUserOps(APIView):
     # GET /api/v1/admin/user/id/:id 	(get one by id)
     def get(self, request, id):
         user = self._get_user_or_404(id)
-        return Response(sers.UserSerializerForAdminPanels(user).data)
+        return utils.uni_response(data=sers.UserSerializerForAdminPanels(user).data)
+        # return Response(sers.UserSerializerForAdminPanels(user).data)
 
     # PUT /api/v1/admin/user/id/:id	(update)
     def put(self, request, id):
         user = self._get_user_or_404(id)
         ser = sers.UserSerializerForAdminPanels(user, 
                 data=request.data, partial=True, context={"user": user})
-        ser.is_valid(raise_exception=True)
+
+        try:
+            ser.is_valid(raise_exception=True)
+        except Exception:
+            return utils.uni_response(
+                data=sers.UserSerializerForAdminPanels(instance).data, 
+                errors={"_": [('update_error','update_error')]}, 
+                status=400)
+        
         instance = ser.save()
-        return Response(sers.UserSerializerForAdminPanels(instance).data)
+        return utils.uni_response(data=sers.UserSerializerForAdminPanels(instance).data)
 
     # DELETE /api/v1/admin/user/id/:id	(delete)
     def delete(self, request, id, format=None):
         user = self._get_user_or_404(id)
 
         if user.id == request.user.id:  # you can't delete your own account in admin panel!
-            raise PermissionDenied()
+            return utils.uni_response(errors={"_": [('cannot_delete_oneself', 'cannot_delete_oneself')]}, status=403)
         
         user.delete()
-        return Response({})
+        return utils.uni_response()
